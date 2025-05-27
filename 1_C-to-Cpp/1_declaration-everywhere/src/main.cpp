52c236c3f5916cf0200b9af24982d174fbbdadc8
Ltsnnh <lt.anh197@gmail.com>
2025-05-27 15:09:10 +0700

    first example

---
+++ 1_C-to-Cpp/1_declaration-everywhere/src/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include "common.h"
+
+int main(void)
+{
+    // declare n
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    // declare *array
+    int *array = (int*)malloc(n*sizeof(int));
+
+    //declare i (NOTE: scope and lifetime of i)
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter value of number %d: ", i+1);
+        scanf("%d", array + i);
+    }
+
+    printf("Array entered: ");
+    //declare i again (NOTE: scope and lifetime of i)
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
