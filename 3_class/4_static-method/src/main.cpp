87a416e97ad89edb88b6239b8df42175fc7148f2
Ltsnnh <lt.anh197@gmail.com>
2025-06-19 14:47:25 +0700

    static method

---
+++ 3_class/4_static-method/src/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <math.h>
+
+class vertex
+{
+    private:
+    float x, y;
+
+    public:
+    void vertexEnter(void)
+    {
+        std::cout << "Enter x, y: ";
+        std::cin >> x >> y;
+        std::cin.ignore(1);
+
+        return;
+    }
+
+    void vertexDisplay(void)
+    {
+        std::cout << "x = " << x << " y = " << y << std::endl;
+
+        return;
+    }
+
+    float vertexSide(vertex B)
+    {
+        return sqrt(pow(this->x - B.x, 2) + pow(this->y - B.y, 2));
+    }
+};
+
+class triangle
+{
+    private:
+    vertex a, b, c;
+
+    public:
+    // static method
+    // no implicit argument this pointer
+    // not associated with any particular object of the class
+    static triangle creatTriangle(vertex A, vertex B, vertex C)
+    {
+        triangle t;
+
+        t.a = A;
+        t.b = B;
+        t.c = C;
+
+        return t;
+    }
+};
+
+int main(void)
+{
+    vertex a, b, c;
+    triangle t;
+
+    a.vertexEnter();
+    b.vertexEnter();
+    c.vertexEnter();
+
+    // method call using class name
+    t = triangle::creatTriangle(a, b, c);
+
+    return 0;
+}
